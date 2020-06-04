7967aaf0d1bd0b7717a540f24d50b36c6fcd2a4b
sagastya <sagastya@yahoo.com>
2020-06-04 01:12:14 -0400

    modified card

--- static/js/app.js
+++ static/js/app.js
@@ -19,36 +19,36 @@ var projects = [
         description: "Correlations",
         url: "https://github.com/Atlchris67/GroupOneProject.git"
     },
-    {
-        name: "NY Citi Bike",
-        language_type: "Tableau",
-        description: "Dashboard",
-        url: "https://tableau.com"
-    },
-    {
-        name: "Seismic Monitor",
-        language_type: "JavaSript",
-        description: "Geo Mapping Earthquakes",
-        url: "https://github.com/sagastya/leaflet-challenge.git"
-    },
-    {
-        name: "UFO Sightings",
-        language_type: "JavaScript",
-        description: "Dashboard",
-        url: "https://tableau.com"
-    },
-    {
-        name: "Destination Hotel",
-        language_type: "Python",
-        description: "Python APIs",
-        url: "github.com/yourrepo"
-    }
+    // {
+    //     name: "NY Citi Bike",
+    //     language_type: "Tableau",
+    //     description: "Dashboard",
+    //     url: "https://tableau.com"
+    // },
+    // {
+    //     name: "Seismic Monitor",
+    //     language_type: "JavaSript",
+    //     description: "Geo Mapping Earthquakes",
+    //     url: "https://github.com/sagastya/leaflet-challenge.git"
+    // },
+    // {
+    //     name: "UFO Sightings",
+    //     language_type: "JavaScript",
+    //     description: "Dashboard",
+    //     url: "https://tableau.com"
+    // },
+    // {
+    //     name: "Destination Hotel",
+    //     language_type: "Python",
+    //     description: "Python APIs",
+    //     url: "github.com/yourrepo"
+    // }
 ]
 
 projects.map(function (project) {
     // console.log(project);
     var projectDiv = document.createElement("div");
-    projectDiv.classList.add("text-center", "col-lg-4", "card")
+    projectDiv.classList.add("rounded", "text-center", "col-lg-4", "card")
     projectDiv.innerHTML = `
     <h5 style="color:#311b92">${project.name}</h5>
     <div>
